0c68c58d2063da88e13ae47cc3471ea400300e3b
RobbieClarken <robbie.clarken@gmail.com>
2017-03-25 15:59:59 +1100

    Fix sacrilegious indentation

--- adxv_load_next.py
+++ adxv_load_next.py
@@ -1,7 +1,7 @@
 """
-Subscribes to a redis pub/sub server which feeds the app with the most 
+Subscribes to a redis pub/sub server which feeds the app with the most
 recent master and data files to be collected. Third field is oscillation/frame
-for calculating the equivalent # of frames for 1 degree of data. 
+for calculating the equivalent # of frames for 1 degree of data.
 """
 
 
@@ -44,47 +44,47 @@ pubsub = redis.pubsub()
 pubsub.subscribe(cfg['sub'])
 
 def talktoadxv(ip, port, slab, slabs, master, h5):
-	"""
-	Sets up a socket connection with a running adxv session.
-	Takes a number of parameters and sends them across the 
-	connection in the stated order. 
-	"""
-	logger.debug('Starting socket connection with arguments:')
-	logger.debug('ip: %s' % (ip))
-	logger.debug('port: %s' % (port))
-	logger.debug('slab: %s' % (slab))
-	logger.debug('slabs: %s' % (slabs))
-	logger.debug('master: %s' % (master))
-	logger.debug('h5: %s' % (h5))
-	a = adxvsocket(ip,int(port))
-	a.set_slab(int(slab))
-	a.increment_slabs()
-	a.load_image(master)
-	a.load_image(h5)
-	a.set_slabs(int(slabs))
-	a.stride(int(slabs))
-	a.raise_window('Image')
-	a.raise_window('Load')
+    """
+    Sets up a socket connection with a running adxv session.
+    Takes a number of parameters and sends them across the
+    connection in the stated order.
+    """
+    logger.debug('Starting socket connection with arguments:')
+    logger.debug('ip: %s' % (ip))
+    logger.debug('port: %s' % (port))
+    logger.debug('slab: %s' % (slab))
+    logger.debug('slabs: %s' % (slabs))
+    logger.debug('master: %s' % (master))
+    logger.debug('h5: %s' % (h5))
+    a = adxvsocket(ip,int(port))
+    a.set_slab(int(slab))
+    a.increment_slabs()
+    a.load_image(master)
+    a.load_image(h5)
+    a.set_slabs(int(slabs))
+    a.stride(int(slabs))
+    a.raise_window('Image')
+    a.raise_window('Load')
 
 # Listen and load loop
 for message in pubsub.listen():
 
-	#Need to filter out first message as it is always '1'.
-	if message['data'] != 1: 
+    #Need to filter out first message as it is always '1'.
+    if message['data'] != 1:
 
-		data_raw = message['data'] # message contains more than the data field
-		data = json.loads(data_raw.decode('utf-8')) # python3 json does not assume text
-		master = data[0] # master file
-		h5 = data[1] # data file
-		slabs = int(1/data[2]) # Osc/frame. Sum until 1 degree.
+        data_raw = message['data'] # message contains more than the data field
+        data = json.loads(data_raw.decode('utf-8')) # python3 json does not assume text
+        master = data[0] # master file
+        h5 = data[1] # data file
+        slabs = int(1/data[2]) # Osc/frame. Sum until 1 degree.
 
-		# Wait until files hit disk
-		while not os.path.exists(h5):
-			logger.info('Waiting for files to hit disk...')
-			sleep(2)
-		
-		# Run function
-		talktoadxv(cfg['ip'], cfg['port'], cfg['slab'], slabs, master, h5)
+        # Wait until files hit disk
+        while not os.path.exists(h5):
+            logger.info('Waiting for files to hit disk...')
+            sleep(2)
+
+        # Run function
+        talktoadxv(cfg['ip'], cfg['port'], cfg['slab'], slabs, master, h5)
 
 #if __name__ == '__main__':
 #    talktoadxv(ip, port, slab, slabs, master, h5)
